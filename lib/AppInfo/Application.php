f69bfb6b9be4fd822ba46d4fb32b94ffe807cf62
Vithujan <vithu1021@hotmail.com>
2024-11-12 22:46:35 +0000

    - Registered the LoginListener as a service in order to utilize the database - Calling createTable() method at the start of the application, so that the table 'rbaa_contextual_user_information' is created if it dosnt exist. - Improved the addListener for userLoggedInEvent to send the event to the handeLogin method

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -8,7 +8,9 @@ namespace OCA\RiskBasedAccountRecovery\AppInfo;
 use OCP\AppFramework\App;
 use OCP\EventDispatcher\IEventDispatcher;
 use OCP\User\Events\UserLoggedInEvent;
+use OCP\IDBConnection;
 
+use OCA\RiskBasedAccountRecovery\Service\DatabaseService;
 use OCA\RiskBasedAccountRecovery\Listener\LoginListener;
 
 //The main application class for the RiskBaseAccountRecovery App.
@@ -17,12 +19,28 @@ class Application extends App {
 
         //Calling the parent constructor to register the app with nextcloud
         parent::__construct('RiskBasedAccountRecovery');
-
+        // Retrieve the container
+        $container = $this->getContainer();
         //Retrieve the event dispatcher from the app container to manage event listeners
-        $dispatcher = $this->getContainer()->query(IEventDispatcher::class);
+        $dispatcher = $container->query(IEventDispatcher::class);
 
-        // Register the LoginListener to handle user login events. When a UserLoggedInEvent occurs, the handleLogin method of LoginListener will be triggered.
-        $dispatcher->addListener(UserLoggedInEvent::class, [new LoginListener(), 'handleLogin']);
+        //Retrieve the IDBConnection from the app container to enable database operations
+        $dbConnection = $container->query(IDBConnection::class);
+
+        //Initialize the DatabaseService class with the database connection
+        $databaseService = new DatabaseService($dbConnection);
+        //Call the createTable() method to create the 'rbaa_contextual_user_information' table if it does not exist
+        $databaseService->createTable();
+
+        // Registering the LoginListener as a service, and injecting the database connection
+        $container->registerService(LoginListener::class, function($c) use ($dbConnection) {
+            return new LoginListener($dbConnection);
+        });
+
+       // Registering the LoginListener to handle user login events. When a UserLoggedInEvent occurs, the handleLogin method of LoginListener will be triggered.
+        $dispatcher->addListener(UserLoggedInEvent::class, function(UserLoggedInEvent $event) use ($container) {
+            $container->get(LoginListener::class)->handleLogin($event);
+        });
 
     }
 
